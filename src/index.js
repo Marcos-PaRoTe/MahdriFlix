e9341ab9df8e788d8a73d3931b7ce9143f5b3060
Marcos-PaRoTe <marcos.paulo.rodrigues@hotmail.com>
2020-08-04 11:49:10 -0300

    Changes 2ª Aula

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home';
+import {BrowserRouter, Switch, Route, } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+
+const Página404 = () => (<div>Página 404, volte a origem por gentileza.</div>) 
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/cadastro/video" exact component={CadastroVideo} /> 
+      <Route component={Página404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
   );
 
